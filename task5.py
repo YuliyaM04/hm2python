2731ed2f2b182d4f9a801e5dcbdae4b249f81c51
Yuliya <fokju@yandex.ru>
2022-09-08 15:05:34 +0300

    Задача5

---
+++ task5.py
@@ -0,0 +1,16 @@
+import random
+
+def get_list(n, x, y):
+    return [random.randint(x, y) for i in range(n)]
+
+def shuffle_list(lst):
+    return random.shuffle(lst)
+
+n = 10
+x = 1
+y = 20
+
+newlist = get_list(n, x, y)
+print(newlist)
+shuffle_list(newlist)
+print(newlist)
